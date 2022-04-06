aed54c61efe9df16c238bf585738081a80c9fa1e
FernandoMacia <fernando.macia@hotmail.es>
2022-04-06 19:01:00 +0200

    Test Numero de un Digito

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -2,4 +2,17 @@ package sumador;
 
 public class ASumar {
 
+	public ASumar() {
+
+	}
+
+	public String sumativo(String cad) {
+        String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        else {
+        	return "Error. Mi código ha fallado...";
+        }
+	}
 }
